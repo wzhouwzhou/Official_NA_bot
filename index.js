4ba29b9e5c5f9598382a01fe07aa4560ff3fba77
Prottay2783 <43699074+Prottay2783@users.noreply.github.com>
2018-09-29 22:55:26 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const config = require("./config.json");
 const database = require("./database.json");
-const botconfig = require("./botconfig.json");
-const tokenfile = require("./token.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
