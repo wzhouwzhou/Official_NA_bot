9fa485003f753516a8b615a7b37b8396b5d9fc2f
Prottay2783 <43699074+Prottay2783@users.noreply.github.com>
2018-09-29 23:53:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const config = require("./config.json");
 const database = require("./database.json");
-const Commando = require("discord.js-commando");
+const Discord = require("discord.js");
 const fs = require("fs");
-const bot = new Commando.Client({disableEveryone: true});
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
 
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commannds");
+fs.readdir("./commands/", (err, files) => {
 
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
 
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
 bot.on("ready", async () =>{
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity(`with 180+ Members`);
