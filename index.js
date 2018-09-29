b7b6f7603fd3562ae1df6e216e2750352fca4da0
Prottay2783 <43699074+Prottay2783@users.noreply.github.com>
2018-09-29 22:54:13 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const config = require("./config.json");
 const database = require("./database.json");
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const botconfig = require("./botconfig.json");
+const tokenfile = require("./token.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
 
 
 
