3ec685b1867b8ae0eadfe9ec1a6a47afc4f91598
Prottay2783 <43699074+Prottay2783@users.noreply.github.com>
2018-09-30 16:27:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -462,5 +462,14 @@ if(command === "removerole"){
     message.reply(`<@${wUser.id}> has been banned.`)
   }
   }
+  if(command === "warnlevel"){
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
+  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+  if(!wUser) return message.reply("Couldn't find them yo");
+  let warnlevel = warns[wUser.id].warns;
+
+  message.reply(`<@${wUser.id}> has ${warnlevel} warnings.`);
+
+  }
 });
 bot.login(config.token);
