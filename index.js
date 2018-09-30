dabe9170b080f83e6ee7093837601d6235089e29
Prottay2783 <43699074+Prottay2783@users.noreply.github.com>
2018-09-30 18:32:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -471,9 +471,9 @@ if(command === "warnings"){
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.reply("Couldn't find them yo");
-  let warnlevel = warns[wUser.id].warns;
+  let warnings = warns[wUser.id].warns;
 
-  message.reply(`<@${wUser.id}> has ${warnlevel} warnings.`);
+  message.reply(`<@${wUser.id}> has ${warnings} warnings.`);
 }
  
   
