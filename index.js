676553eb45ad2902e6b64cebe1fa3cbb2d1e4ac0
Prottay2783 <43699074+Prottay2783@users.noreply.github.com>
2018-09-30 13:18:20 +0600

    Update index.js

--- index.js
+++ index.js
@@ -349,7 +349,7 @@ if(command === "mute") {
   }
   
   if(command === "addrole"){
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
+  if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Sorry pal, you can't do that.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Couldn't find that user, yo.");
   let role = args.join(" ").slice(22);
