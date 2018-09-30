2e417897239b7cc4ad388f60ef742c864e8b3d36
Prottay2783 <43699074+Prottay2783@users.noreply.github.com>
2018-09-30 17:54:22 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const ms = require("ms");
 const fs = require("fs");
 let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
-
 bot.on("ready", async () =>{
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity("with 180+ Members");
