3a742f96b9cef610c3787ec56807119efe6391f4
Prottay2783 <43699074+Prottay2783@users.noreply.github.com>
2018-10-01 11:17:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ bot.on("message", async message => {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
     // Please read on Array.some() to understand this bit:
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["✝Nitro✝", "╰☆ Head of All Staffs ☆╮", "╰☆ +ИД | Senior Staff Team ☆╮", "╰☆ +ИД | Staff Team ☆╮", "╰☆ +ИД | Intern ☆╮"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["✝Nitro✝", "░+ИД | Cø-Øwners░", "╰☆ Head of All Staffs ☆╮", "╰☆ +ИД | Senior Staff Team ☆╮", "╰☆ +ИД | Staff Team ☆╮", "╰☆ +ИД | Intern ☆╮"].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
 
     // Let's first check if we have a member and if we can kick them!
@@ -259,7 +259,7 @@ bot.on("message", async message => {
   if(command === "ban") {
     // Most of this command is identical to kick, except that here we'll only let admins do it.
     // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["✝Nitro✝", "╰☆ Head of All Staffs ☆╮", "╰☆ +ИД | Senior Staff Team ☆╮"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["✝Nitro✝", "░+ИД | Cø-Øwners░", "╰☆ Head of All Staffs ☆╮", "╰☆ +ИД | Senior Staff Team ☆╮"].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
 
     let member = message.mentions.members.first();
