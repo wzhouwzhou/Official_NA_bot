633150bff1613b0d42b9f74cb2fa79fc6d6a1aef
Prottay2783 <43699074+Prottay2783@users.noreply.github.com>
2018-10-05 16:00:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -687,6 +687,23 @@ let tossembed = new Discord.RichEmbed()
 
 
 message.channel.send(tossembed);
-}  
+}
+if(command === "coins"){
+if(!coins[message.author.id]){
+    coins[message.author.id] = {
+      coins: 0
+    };
+  }
+
+  let uCoins = coins[message.author.id].coins;
+
+
+  let coinEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#00FF00")
+  .addField("💸", uCoins);
+
+  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+}
 });
 bot.login(process.env.token);
