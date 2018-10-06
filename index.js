5593795825340c6528188d0bf99e8e5b7aaff697
Prottay2783 <43699074+Prottay2783@users.noreply.github.com>
2018-10-06 14:08:15 +0600

    Update index.js

--- index.js
+++ index.js
@@ -708,17 +708,4 @@ if(!coins[message.author.id]){
   
 }
 });
-function $play(connection, message){
-    var server = servers[message.guild.id];
-
-    server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: 
-"audioonly"}));
-
-    server.queue.shift();
-
-    server.dispatcher.on("end", function() {
-        if(server.queue[0]) play(connection, message);
-        else connection.disconnect();
-    });
-}
 bot.login(process.env.token);
