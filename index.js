e80cc9d6af8333c0ffca2818fc6582b1533560c8
Prottay2783 <43699074+Prottay2783@users.noreply.github.com>
2018-10-10 21:51:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -750,7 +750,7 @@ if(!coins[message.author.id]){
 }
 
 if (command === "poll"){
- let args = message.content.match(/"(.+?)"/g);
+let args = message.content.match(/"(.+?)"/g);
     if (args) {
       if (!canSendPoll(message.author.id)) {
         return message
@@ -774,21 +774,21 @@ if (command === "poll"){
         const question = args[0];
         const questionOptions = [...new Set(args.slice(1))];
         if (questionOptions.length > 20) {
-          return message
-            .channel
-            .send(`${message.author} Polls are limited to 20 options.`);
+          return message.channel.send(`${message.author} Polls are limited to 20 options.`);
         } else {
           pollLog[message.author.id] = {
             lastPoll: Date.now()
           };
           let pollembed = new Discord.RichEmbed()
+
           .setThumbnail(`http://www.vectorfree.com/media/vectors/raised-fist.jpg`)
+
           .setColor("#17dbd6")
+
           .addField(`${question}`, `${questionOptions
+
     .map((option, i) => `${options[i]} - ${option}`).join('\n')}`);
-          
-          return 
-          message
+          return message
             .channel
             .send(pollembed)
             .then(async (pollMessage) => {
